55417261eb329b78fb48a475714fe39f631b97cb
GABRIEL MARTINS VIEIRA <gabriel.vieira2@alunos.sc.senac.br>
2023-10-03 10:42:22 -0300

    Alinhando a aula

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { RequisicaoService } from 'src/app/service/requisicao.service';
+import { LoadingController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  public nome:string = '';
+  public login:string = '';
+  public senha:string = '';
+
+  constructor(
+    public requisicao_service:RequisicaoService,
+    private loadingController: LoadingController
+  ) {}
 
   ngOnInit() {
+  }
+
+  async salvar(){
+
+    const loading = await this.loadingController.create({
+      message: 'Salvando...',
+    });
+
+    await loading.present();    
+
+    const fd = new FormData();
+    fd.append('controller','usuario');
+    fd.append('nome',this.nome);
+    fd.append('login',this.login);
+    fd.append('senha',this.senha);
+
+    this.requisicao_service
+    .post(fd)
+    .subscribe(
+      async () => {
+        await loading.dismiss();
+      }      
+    );
+
   }
 
 }
